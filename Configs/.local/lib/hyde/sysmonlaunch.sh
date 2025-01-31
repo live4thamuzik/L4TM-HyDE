0369de98e51d7ccbb82682ca4478a77f0de8d2f6
Steve <98226771+live4thamuzik@users.noreply.github.com>
2025-01-31 18:40:25 -0500

    Initial commit

---
+++ Configs/.local/lib/hyde/sysmonlaunch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+scrDir="$(dirname "$(realpath "$0")")"
+# shellcheck disable=SC1091
+source "${scrDir}/globalcontrol.sh"
+pkgChk=("io.missioncenter.MissionCenter" "htop" "btop" "top")
+pkgChk+=("${SYSMONITOR_COMMANDS[@]}")
+
+for sysMon in "${!pkgChk[@]}"; do
+    if [ "${sysMon}" -gt 0 ]; then
+        term=$(grep -E '^\s*'"$term" "$HOME/.config/hypr/keybindings.conf" | cut -d '=' -f2 | xargs) # dumb search the config
+        term=${TERMINAL:-$term}                                                                      # Use env var
+        term=${SYSMONITOR_TERMINAL:-$term}                                                           # use the declared variable
+    fi
+
+    if pkg_installed "${pkgChk[sysMon]}"; then
+        pkill -x "${pkgChk[sysMon]}" || ${term} "${pkgChk[sysMon]}" &
+        break
+    fi
+done
