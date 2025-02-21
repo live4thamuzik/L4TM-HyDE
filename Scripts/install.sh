59347409a2138851667c2f62de320795551b5b38
Steve <live4thamuzik@gmail.com>
2025-02-21 14:25:29 -0500

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -135,17 +135,17 @@ EOF
     #--------------------------------#
     # add nvidia drivers to the list #
     #--------------------------------#
-    if nvidia_detect; then
-        if [ ${flg_Nvidia} -eq 1 ]; then
-            cat /usr/lib/modules/*/pkgbase | while read -r kernel; do
-                echo "${kernel}-headers" >>"${scrDir}/install_pkg.lst"
-            done
-            nvidia_detect --drivers >>"${scrDir}/install_pkg.lst"
-        else
-            print_log -warn "Nvidia" " :: " "Nvidia GPU detected but ignored..."
-        fi
-    fi
-    nvidia_detect --verbose
+    #if nvidia_detect; then
+    #    if [ ${flg_Nvidia} -eq 1 ]; then
+    #        cat /usr/lib/modules/*/pkgbase | while read -r kernel; do
+    #            echo "${kernel}-headers" >>"${scrDir}/install_pkg.lst"
+    #        done
+    #        nvidia_detect --drivers >>"${scrDir}/install_pkg.lst"
+    #    else
+    #        print_log -warn "Nvidia" " :: " "Nvidia GPU detected but ignored..."
+    #    fi
+    #fi
+    #nvidia_detect --verbose
 
     #----------------#
     # get user prefs #
